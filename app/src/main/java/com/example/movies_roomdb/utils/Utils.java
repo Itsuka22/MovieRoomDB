a7ffe02a59c3a08b2a14edd17d1965329f375670
Itsuka22 <111201911829@mhs.dinus.ac.id>
2021-10-04 21:29:43 +0700

    update

--- app/src/main/java/com/example/movies_roomdb/utils/Utils.java
+++ app/src/main/java/com/example/movies_roomdb/utils/Utils.java
@@ -1,5 +1,5 @@
 package com.example.movies_roomdb.utils;
 
 public class Utils {
-    public static final String API_KEY="f7c34fa95d70bb2d4395963bcfb293ca";
+    public static final String API_KEY="0dde3e9896a8c299d142e214fcb636f8";
 }
